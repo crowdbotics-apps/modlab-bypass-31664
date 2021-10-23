b4956c688e2b1196804b030a71bcd60fff53c206
Crowdbotics <team@crowdbotics.com>
2021-10-23 15:19:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "modlab_bypass_31664",
+  url: "https://modlab_bypass_31664.botics.co",
+  api: "https://modlab_bypass_31664.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
